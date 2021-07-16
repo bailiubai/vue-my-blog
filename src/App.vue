2e66948241e07535980b079793b73b338a52e7ee
bailiubai <573627684@qq.com>
2021-07-16 17:23:39 +0800

    blog tijiao

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,23 @@
 <template>
   <div id="app">
-    <add-blog></add-blog>
+    <!-- <add-blog></add-blog> -->
+    <show-blogs></show-blogs>
   </div>
 </template>
 
 <script>
 import AddBlog from './components/AddBlog.vue'
+import ShowBlogs from './components/ShowBlogs.vue'
 
 export default {
   name: 'App',
   components: {
-    AddBlog
+    AddBlog,
+    ShowBlogs
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/ShowBlogs.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="show-blogs">
+    <h1>Show - Blogs</h1>
+    <div v-for="blog in blogs" class="single-blog">
+        <h2>{{blog.title}}</h2>
+        <article>
+            {{blog.body}}
+        </article>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'show-blogs',
+  data(){
+      return{
+        blogs:[]
+      }
+  },
+  created(){
+      this.$http.get('http://localhost:8888/blogs').then(function(data){
+          console.log(data)
+          this.blogs = data.body.slice(0,9);
+          console.log(this.blogs)
+      })
+  }
+}
+</script>
+
+<style>
+#show-blogs{
+    max-width: 800px;
+    margin: 0 auto;
+}
+
+.single-blog{
+    padding: 20px;
+    margin: 20px 0;
+    box-sizing: border-box;
+    background: #eee;
+}
+</style>
